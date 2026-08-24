697dd58c8aa65092f8e58e713702bb2404bbe49f
Marcos Vladimir Diaz Peralta <20260928@itla.edu.do>
2026-08-23 22:11:53 -0400

    01: Hola Mundo y presentacion

---
+++ 01-HolaMundo/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Nombre: Marcos Diaz
+ * Fecha: 23/08/2026
+ * Descripcion: El programa muestra un mensaje en consola con mi nombre, matricula, carrera y una frase que me define
+ */
+
+
+// use el console.writeLine para mostrar el mensaje en consola y dejar un espacio entre cada linea para que se vea mas bonito
+// use simbolos como el de = para hacer un marco alrededor del mensaje
+Console.WriteLine("*==============================================================*");
+Console.WriteLine("|                                                              |");
+Console.WriteLine("|              Nombre:     Marcos Diaz                         |");
+Console.WriteLine("|              Matricula:  2026-0928                           |");
+Console.WriteLine("|              Carrera:    Desarrollo de Software              |");
+Console.WriteLine("|                                                              |");
+// use el /" para mostrar comillas dentro del mensaje, ya que no me dejaba mostrarlo porque el lenguaje leia las comillas como el final del mensaje
+Console.WriteLine("|   \"Soy un oceano de conocimiento con un cm de profundidad\" |");
+Console.WriteLine("|                                                              |");
+Console.WriteLine("*==============================================================*");
